e74dd1e7a9c8640e504b6be4652534eac0e5f870
Shiakh219 <bilalbagmar@gmail.com>
2025-10-22 09:34:31 +0530

    Removed deprecated appDir option

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true // Enable if you're using the App Router
-  },
   images: {
-    domains: [] // Add image domains here if needed
-  }
+    domains: [], // Add external image domains if needed
+  },
 };
 
 module.exports = nextConfig;
